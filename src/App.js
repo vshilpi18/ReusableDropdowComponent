7781f9ce9f29a12d649409f5ff249190e12ff985
vshilpi18 <43678084+vshilpi18@users.noreply.github.com>
2019-07-20 22:32:20 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import Dropdown from './Dropdown';
-import {getDropdownData} from './api';
-export default class App extends React.Component{
+import { getDropdownData } from './api';
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.ddlList = this.ddlList.bind(this);
     this.state = {
-      options: {}
+      options: {},
+      name: ''
     }
   }
-  componentDidMount(){
-    this.ddlList();
+  componentDidMount() {
+    this.ddlList();//Is the api to fetch employee data
   }
   render() {
     return (
       <div className="App">
+        {/* This dropdown can be used anywaher in any component*/}
         <Dropdown
-          id          = {'employeeList'}
-          name        = {'employeeList'}
-          options     = {this.state.options}
-          labelName   = {'Employees List'}
+          id        = {'employeeList'}
+          name      = {'employeeList'}
+          options   = {this.state.options}
+          labelName = {'Employees List'}
+          onchange  = {this.getdata}
         />
+        <div className="drp_dd3">
+            <label>Name</label> : &nbsp;
+            <input value={this.state.name} placeholder="Name" />
+        </div>
       </div>
     );
   }
+  getdata = (e) => {
+    var sel = e.target;
+    var sv = sel.options[sel.selectedIndex].text;
+    this.setState({
+        name: sv
+    })
+  }
   ddlList = () => {
-    getDropdownData( (res) => {
-      let arr = [];
-      let options = res.map((data,id) => {
+    getDropdownData((res) => {
+      let options = res.map((data, id) => {
+        let arr = [];
         arr['id'] = data.id;
-        arr['name'] = data.employee_name;
+        arr['name'] = data.name;
         return arr;
       })
-      this.setState({options:options});
+      this.setState({ options: options });
     });
   }
+
 }
 
