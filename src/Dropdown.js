737277d45afa7755ce51ad07c77e589cbdfde0af
vshilpi18 <43678084+vshilpi18@users.noreply.github.com>
2019-07-20 22:31:45 +0530

    updated dropdown.js

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -2,24 +2,31 @@ import React from 'react';
 
 export default class Dropdown extends React.Component {
     render() {
-        const {name,id,classes,options,labelName} = this.props;
-        return(
+        const { name, id, classes, options, labelName, onchange } = this.props;
+        return (
             <div>
-                <label htmlFor={id}>{labelName}</label>
-                <select name={name} id={id} className={classes}>
-                    <option>Select an option</option>
-                    {
-                        (options !== undefined && options.length > 0)
-                        ?
-                        options.map(option => (
-                        <option key={option.id} value={option.id}>
-                            {option.name}
-                        </option>
-                        ))
-                        :
-                        ''
-                    }
-                </select>
+                {/* It is a reusable component for dropdown.Throughout the project it can b use.
+                */}
+                <div>
+                <label style={{"float":"center"}} className="drp_dd">Dynamic Dropdown</label>
+                </div>
+                <div className="drp_dd2">
+                    <label htmlFor={id}>{labelName}</label>  &nbsp; &nbsp;
+                    <select name={name} className={classes} onChange={onchange}>
+                        <option>Select an option</option>
+                        {
+                            (options !== undefined && options.length > 0)
+                                ?
+                                options.map(option => (
+                                    <option key={option.id} value={option.id}>
+                                        {option.name}
+                                    </option>
+                                ))
+                                :
+                                ''
+                        }
+                    </select>
+                </div>
             </div>
         );
     }
