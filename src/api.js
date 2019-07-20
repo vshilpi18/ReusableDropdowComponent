bb1f6dc4ded47c1459c3179866c888234e3c9c34
vshilpi18 <43678084+vshilpi18@users.noreply.github.com>
2019-07-20 22:39:59 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 export function getDropdownData(callback) {
-    axios.get('http://dummy.restapiexample.com/api/v1/employees').then(function (response){
+    //Dummy api is being called to fetch data
+     axios.get('https://jsonplaceholder.typicode.com/users').then(function(response){
         callback(response.data);
     }).catch(function(error){
         console.log(error);
